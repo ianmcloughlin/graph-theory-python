fbec5adc2191121a2ee468ae70ebd096de2a133a
Ian McLoughlin <mcloughlin.ian@gmail.com>
2021-01-30 13:33:03 +0000

    Looked at the basics.

---
+++ basics.py
@@ -0,0 +1,14 @@
+# Ian McLoughlin
+# Python basics
+# 2021-01-30
+
+print("Hello, world!")
+
+a = 1
+b = 1.0
+s = "Hello, world from a string!"
+t = 'Hello, from a different string'
+
+print(a, b, s, t)
+
+print(s[3:10:2])  
